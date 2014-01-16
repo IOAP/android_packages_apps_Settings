b96e401f1e4e2ce317fc77d348580739ddad5c30
mrimp <mrimp@day-off.net>
2014-01-15 20:39:05 -0500

    few layout changes removed AS idea added App Multi Select List Preference

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -65,12 +65,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
-    private static final String KEY_DEVICE_CPU = "device_cpu";
-    private static final String KEY_DEVICE_GPU = "device_gpu";
-    private static final String KEY_DEVICE_MEMORY = "device_memory";
-    private static final String KEY_DEVICE_REAR_CAMERA = "device_rear_camera";
-    private static final String KEY_DEVICE_FRONT_CAMERA = "device_front_camera";
-    private static final String KEY_DEVICE_SCREEN_RESOLUTION = "device_screen_resolution";
+
 
     long[] mHits = new long[3];
 
@@ -101,18 +96,6 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         setValueSummary(KEY_MOD_VERSION, "ro.modversion");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
-
-        addStringPreference(KEY_DEVICE_CPU,
-                SystemProperties.get("ro.device.cpu", getCPUInfo()));
-        addStringPreference(KEY_DEVICE_GPU,
-                SystemProperties.get("ro.device.gpu", null));
-        addStringPreference(KEY_DEVICE_MEMORY, getMemInfo());
-        addStringPreference(KEY_DEVICE_FRONT_CAMERA,
-                SystemProperties.get("ro.device.front_cam", null));
-        addStringPreference(KEY_DEVICE_REAR_CAMERA,
-                SystemProperties.get("ro.device.rear_cam", null));
-        addStringPreference(KEY_DEVICE_SCREEN_RESOLUTION,
-                SystemProperties.get("ro.device.screen_res", null));
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
