5f5797f7d842011d60f0e387829127e980dfce1d
mrimp <mrimp@day-off.net>
2014-03-16 09:48:09 -0400

    bumped to v3.57 upstream updates

--- src/com/android/settings/cyanogenmod/ProgressBar.java
+++ src/com/android/settings/cyanogenmod/ProgressBar.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2013-2014 Dokdo Project - Gwon Hyeok
- * Copyright (C) 2014 IOAP Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
